ae13ea0b03583870d6285a54433d1aca8f6e0fbb
Jaka Furlan <jaka.furlan@cosylab.com>
2025-11-27 19:04:53 +0100

    hi

---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import redovalnica "github.com/jf76272/PorazdeljeniSistemi/redovalnica"
+
+var r = map[string]redovalnica.Student{
+	"63230069": redovalnica.Student{"Jaka", "Furlan", []int{10, 10, 10, 10, 10, 10, 10, 10}},
+	"63230070": redovalnica.Student{"David", "Jagodic", []int{7, 10, 8, 9, 5, 5, 8}},
+	"63230071": redovalnica.Student{"Tilen", "Gorsek", []int{7, 8, 9, 10, 7, 8, 6}},
+	"63230072": redovalnica.Student{"Tim", "Lipnik", []int{9, 9, 7, 6, 8}},
+}
+
+func main() {
+
+}
